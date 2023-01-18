bdfa4e0fa0597c35e6950267b21b390c22f0cee3
RyuyaArai <k020g1223@g.neec.ac.jp>
2023-01-18 16:09:02 +0900

    カメラ途中

--- Assets/Resources/Player/PlayerCamera/CameraMove.cs
+++ Assets/Resources/Player/PlayerCamera/CameraMove.cs
@@ -15,22 +15,30 @@ public class CameraMove : MonoBehaviour
     private float cameraRotateSpeed = 90f;
     //　カメラのキャラクターからの相対値を指定
     [SerializeField]
-    private Vector3 basePos = new Vector3(0f, 1f, 2.5f);
+    private Vector3 basePos = new Vector3(0f, 2f, 4f);
     // 障害物とするレイヤー
     [SerializeField]
     private LayerMask obstacleLayer;
  
     private bool isPause;
 
-    void Update() {
+    private Vector3 charaPos;
+
+    private void Start() {
+        charaPos = charaLookAtPosition.position;
+    }
+
+    private void Update() {
         isPause = MenuManager.instance.GetIsPause();
         
         if(!isPause) {
             //　通常のカメラ位置を計算
-            var cameraPos = charaLookAtPosition.position + (-charaLookAtPosition.forward * basePos.z) + (Vector3.up * basePos.y);
+            //var cameraPos = charaLookAtPosition.position + (-charaLookAtPosition.forward * basePos.z) + (Vector3.up * basePos.y);
             //　カメラの位置をキャラクターの後ろ側に移動させる
             //transform.position = Vector3.Lerp(transform.position, cameraPos, cameraMoveSpeed * Time.deltaTime);
-            transform.LookAt(charaLookAtPosition.position);
+            //transform.LookAt(charaLookAtPosition.position);
+            //transform.position += charaLookAtPosition.position - charaPos;
+            //charaPos = charaLookAtPosition.position;
 
             // // マウスの移動量を取得
             float mx = Input.GetAxis("Mouse X");
@@ -40,14 +48,16 @@ public class CameraMove : MonoBehaviour
             if (Mathf.Abs(mx) > 0.00001f)
             {
                 // 回転軸はワールド座標のY軸
-                transform.RotateAround(charaLookAtPosition.transform.position, Vector3.up, mx);
+                //transform.RotateAround(charaPos, Vector3.up, mx * Time.deltaTime * 200f);
+                transform.position.x = charaLookAtPosition.position.x + Mathf.Cos(Mathf.PI * mx / 180) * 5;
             }
     
             // Y方向に一定量移動していれば縦回転
             if (Mathf.Abs(my) > 0.00001f)
             {
                 // 回転軸はカメラ自身のX軸
-                transform.RotateAround(charaLookAtPosition.transform.position, transform.right, my);
+                //transform.RotateAround(charaPos, transform.right, -my * Time.deltaTime * 200f);
+                transform.position.y = charaLookAtPosition.position.y + Mathf.Sin(Mathf.PI * my / 180) * 2;
             }
 
             RaycastHit hit;
