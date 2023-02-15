ffb02c696c259adb1286af9ffc64f597e4250896
RyuyaArai <k020g1223@g.neec.ac.jp>
2023-02-15 15:48:05 +0900

    カメラ終わり

--- Assets/Resources/Player/PlayerCamera/CameraMove.cs
+++ Assets/Resources/Player/PlayerCamera/CameraMove.cs
@@ -7,46 +7,49 @@ public class CameraMove : MonoBehaviour
  //　キャラクターのTransform
     [SerializeField]
     private Transform charaLookAtPosition;
-    //　カメラの移動スピード
-    [SerializeField]
-    private float cameraMoveSpeed = 2f;
     //　カメラの回転スピード
     [SerializeField]
     private float cameraRotateSpeed = 90f;
     //　カメラのキャラクターからの相対値を指定
     [SerializeField]
-    private Vector3 basePos = new Vector3(0f, 2f, 4f);
+    private Vector3 basePos;
     // 障害物とするレイヤー
     [SerializeField]
     private LayerMask obstacleLayer;
  
+    private float cameraMoveSpeed = 5; 
+
     private bool isPause;
 
-    private Vector3 charaPos;
-
     private void Start() {
-        charaPos = charaLookAtPosition.position;
     }
 
-    private void Update() {
+    private void FixedUpdate() {
         isPause = MenuManager.instance.GetIsPause();
         
         if(!isPause) {
             //　通常のカメラ位置を計算
-            var cameraPos = charaLookAtPosition.position + (-charaLookAtPosition.forward * basePos.z) + (Vector3.up * basePos.y);
+            var cameraPos = charaLookAtPosition.position + (-transform.forward * basePos.z) + (Vector3.up * basePos.y);
             //　カメラの位置をキャラクターの後ろ側に移動させる
-            transform.position = Vector3.Lerp(transform.position, cameraPos, cameraMoveSpeed * Time.deltaTime);
 
             // // マウスの移動量を取得
             float mx = Input.GetAxis("Mouse X");
             float my = Input.GetAxis("Mouse Y");
     
+            RaycastHit hit;
+            //　キャラクターとカメラの間に障害物があったら障害物の位置にカメラを移動させる
+            if (Physics.Linecast(charaLookAtPosition.position, transform.position, out hit, obstacleLayer)) {
+                transform.position = hit.point;
+            }
+            else if(distance <= 5) {
+                transform.position = Vector3.Lerp(transform.position, cameraPos, cameraMoveSpeed * Time.deltaTime);
+            }
+
             // X方向に一定量移動していれば横回転
             if (Mathf.Abs(mx) > 0.00001f)
             {
                 // 回転軸はワールド座標のY軸
                 transform.RotateAround(charaLookAtPosition.position, Vector3.up, mx);
-              
             }
     
             // Y方向に一定量移動していれば縦回転
@@ -54,14 +57,8 @@ public class CameraMove : MonoBehaviour
             {
                 // 回転軸はカメラ自身のX軸
                 transform.RotateAround(charaLookAtPosition.position, transform.right, -my);
-
             }
 
-            RaycastHit hit;
-            //　キャラクターとカメラの間に障害物があったら障害物の位置にカメラを移動させる
-            if (Physics.Linecast(charaLookAtPosition.position, transform.position, out hit, obstacleLayer)) {
-                transform.position = hit.point;
-            }
             //　レイを視覚的に確認
             Debug.DrawLine(charaLookAtPosition.position, transform.position, Color.red, 0f, false);
     
